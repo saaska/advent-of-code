cac18dd669df6d3c2c7f5b84aa005637ff8b8e37
saaska <26275111+saaska@users.noreply.github.com>
2021-12-29 17:03:42 +0900

    06-2 in Python

---
+++ 2021/06-Lanternfish/lanternfish-02.py
@@ -0,0 +1,21 @@
+# start: 2021-12-29T07:57:20Z
+
+from collections import Counter
+a = list(map(int, input().split(',')))
+c = Counter(a)
+
+for day in range(256):
+	print(f"day {day}: {c}")
+	d = {}
+	for days in range(8):
+		d[days] = c.get(days+1, 0)
+	d[8] = c.get(0,0)
+	d[6] += c.get(0,0)
+	c = d
+
+print(sum(c.values()))
+
+# end: 2021-12-29T08:02:24Z
+
+
+
