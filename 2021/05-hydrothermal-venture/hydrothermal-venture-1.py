c2023f1727a383a5ae35afb8425cc710688d7654
saaska <26275111+saaska@users.noreply.github.com>
2021-12-29 16:08:59 +0900

    05-1 in Python

---
+++ 2021/05-hydrothermal-venture/hydrothermal-venture-1.py
@@ -0,0 +1,25 @@
+# start: 2021-12-29T06:04:51Z
+
+import numpy as np
+
+L = open('input.txt').read().replace(' -> ',',').replace('\n',',')
+P = np.fromstring(L, sep=',', dtype=np.int16).reshape(-1,4)
+
+W,H = P[:,1::2].max(), P[:,::2].max()
+Z = np.zeros((H,W), dtype=np.uint16)
+
+print(len(Z[Z>1]))
+
+for row in P:
+	j,i,J,I = row
+	if J<j:
+		j,J = J,j
+	if I<i:
+		i,I = I,i
+	if (I-i)>0 and (J-j)>0: continue
+	Z[i:I+1, j:J+1] += 1
+	print(len(Z[Z>1]))
+
+print(len(Z[Z>1]))
+
+#end: 2021-12-29T07:08:30Z
